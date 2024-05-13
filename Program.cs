10ecbee381e10b6652644e407dd3a6bf0a89eec5
Juan Pinero <127610369+HalcyonDeveloper@users.noreply.github.com>
2024-05-13 11:18:37 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Hi_Tech_Order_Managment.GUI.Login;
+using System;
+using System.Windows.Forms;
+
+namespace Hi_Tech_Order_Managment
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainLogin());
+        }
+    }
+}
